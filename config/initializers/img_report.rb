26e6d79246761acbb251d1c7f47f77f0b967b3f2
minhyun lee <leeminhyun0613@gmail.com>
2017-10-14 13:39:15 +0000

    my first redmap

---
+++ config/initializers/img_report.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     '',                        # required
+    aws_secret_access_key: '',                        # required
+    region:                'ap-northeast-2',                  # soptional, defaults to 'us-east-1'
+    endpoint:              'https://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
+  }
+  config.fog_directory  = 'imgreport'                          # required
+  config.fog_public     = true                                        # optional, defaults to true
+  config.fog_attributes = {  } # optional, defaults to {}
+end
